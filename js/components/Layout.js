3b8678d5b63f7b83464b8ffdaaeb4916379e7d9e
depalab <deparesearch@gmail.com>
2025-02-20 15:22:12 -0500

    Update Layout.js

--- js/components/Layout.js
+++ js/components/Layout.js
@@ -1,18 +1,14 @@
-// Layout.js
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-
+// js/components/Layout.js
 const Layout = ({ children }) => {
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen pt-16">
-        {children}
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <div className="layout-wrapper">
+            <Header />
+            <main className="min-h-screen pt-16">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
-export default Layout;
+window.Layout = Layout;
