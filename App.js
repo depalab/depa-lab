903fb3e9672665f0d2edc85cedbacc645739f78b
depalab <deparesearch@gmail.com>
2025-02-20 09:37:06 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import Layout from './components/Layout';
+
+const App = () => {
+  return (
+    <Layout>
+      {/* Your page content goes here */}
+    </Layout>
+  );
+};
+
+export default App;
