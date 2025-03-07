7e32648ddbc73872e6d46dfba771a1517ffb00db
depalab <deparesearch@gmail.com>
2025-03-07 13:43:33 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: "Benjamin Hall", role: "Researcher", image: "images/Benjamin Hall.jpg" },
         { name: "Emmanuel Masa-ibi", role: "Researcher", image: "images/Emmanuel Masa-ibi.jpeg" },
         { name: "Awotwi Baffoe", role: "AI Researcher", image: "images/Awotwi_Baffoe.jpg" },
-        { name: "Opeyemi Adeniran", role: "AI Researcher", image: "images/opeyemi.JPG" },
+        { name: "Opeyemi Adeniran", role: "AI Researcher", image: "images/Opeyemi.PNG" },
         { name: "Anjolie Anthony", role: "Researcher", image: "images/Anjolie.JPG" },
         { name: "Binisa Giri", role: "AI Researcher", image: "images/giri-1.png" },
         { name: "David Nyarko", role: "AI Researcher", image: "images/david-nyarko.JPG" }
